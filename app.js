c1409739792cbebb372f8951cfa60a60dad3b0dc
shwetaps605 <shwetaroy305@gmail.com>
2022-01-18 20:02:47 +0530

    Needs list_id to wor further

--- app.js
+++ app.js
@@ -1,42 +1,51 @@
 const express = require('express')
 const request = require('request')
 const bodyParser = require('body-parser')
-const path = require('path')
-
+const path = require('path');
 const app = express();
 const port = process.env.port || 5000;
 
 //Static folder
 app.use(express.static(path.join(__dirname, 'public')))
+
+//Middleware
 app.use(bodyParser.urlencoded({ extended: false }))
 
 //Signup route
 app.post('/signup', (req, res) => {
     const { firstName, lastName, email } = req.body
-    const mailchimpData = {
+    const data = {
         members: [
             {
                 email_address: email,
-                status: 'pending'
+                status: 'pending',
+                merge_fields:{
+                    FNAME:firstName,
+                    LNAME:lastName
+                }
             }
         ]
     }
-    const mcDataPost = JSON.stringify(mailchimpData)
+    const mcDataPost = JSON.stringify(data)
+    const list_id ="Enter your list id here"
     const options = {
-        url: "",
+        url: `https://us5.api.mailchimp.com/3.0/lists/${list_id}`,
         method: "POST",
         headers: {
-            Authorization: 'auth ..'
+            Authorization: 'auth 1b1a2f335d207411b8ccb095fb3e9bf0-us5'
         },
         body: mcDataPost
     }
-    if (email) {
-        //successful so far
+
+
+    if (email && firstName && lastName) {
         request(options, (err, response, body) => {
             if(err){
                 res.json({error:err})
             } else {
-                res.sendStatus(200)
+                if(response.statusCode === 200){
+                     res.status(200).send("You have to check your email to confirm")
+                }
             }
         })
     } else {
